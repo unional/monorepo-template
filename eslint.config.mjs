7355b43dfead083e0d93c5b482cecddc60f6d28a
Homa Wong <hwong@paloaltonetworks.com>
2025-04-24 15:39:47 -0700

    chore: update repo deps

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { includeIgnoreFile } from '@eslint/compat'
+import formatjs from 'eslint-plugin-formatjs'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tsEslint from 'typescript-eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	includeIgnoreFile(gitignorePath),
+	{ languageOptions: { globals: globals.browser } },
+	tsEslint.configs.base,
+	tsEslint.configs.eslintRecommended,
+	{
+		plugins: {
+			formatjs
+		},
+		rules: {
+			'formatjs/enforce-description': ['error', 'literal'],
+			'formatjs/enforce-default-message': ['error', 'literal'],
+			'formatjs/enforce-id': [
+				'error',
+				{
+					idInterpolationPattern: '[sha512:contenthash:base64:6]'
+				}
+			],
+			'formatjs/no-multiple-whitespaces': 'error',
+			'formatjs/no-multiple-plurals': 'error'
+		}
+	}
+]
